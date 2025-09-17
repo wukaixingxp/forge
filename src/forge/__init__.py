ebed2902805b9b48cc8e817816191d1fb2e29200
Felipe Mello <fmellomascarenhas@gmail.com>
2025-09-16 20:36:35 -0400

    add hf_transfer (faster model download from hf) (#164)
    * add hf_transfer
    
    * linter
    
    ---------
    
    Co-authored-by: Felipe Mello <felipemello@fb.com>

--- src/forge/__init__.py
+++ src/forge/__init__.py
@@ -3,3 +3,15 @@
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
+
+# Enables faster downloading. For more info: https://huggingface.co/docs/huggingface_hub/en/guides/download#faster-downloads
+# To disable, run `HF_HUB_ENABLE_HF_TRANSFER=0 tune download <model_config>`
+try:
+    import os
+
+    import hf_transfer  # noqa
+
+    if os.environ.get("HF_HUB_ENABLE_HF_TRANSFER") is None:
+        os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
+except ImportError:
+    pass
