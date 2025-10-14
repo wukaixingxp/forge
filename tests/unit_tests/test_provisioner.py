399b20dd950eb17861b7d8995b4287a2833be35e
ebsmothers <ebs@fb.com>
2025-10-14 09:43:28 -0700

    GPU CI (#334)

--- tests/unit_tests/test_provisioner.py
+++ tests/unit_tests/test_provisioner.py
@@ -144,7 +144,7 @@ class TestProvisionerCudaVisibleDevices:
         available = local_gpu_manager.get_available_gpus()
         assert available == [str(i) for i in range(8)]
 
-    @mock.patch.dict(os.environ, {"CUDA_VISIBLE_DEVICES": "0,1,2"}, clear=True)
+    @mock.patch.dict(os.environ, {"CUDA_VISIBLE_DEVICES": "0,1,2"}, clear=False)
     @pytest.mark.asyncio
     async def test_get_proc_mesh_respects_cuda_visible_devices(self):
         """Test that get_proc_mesh uses CUDA_VISIBLE_DEVICES for local allocation."""
